72ee6c554a0fc68522e0e4d1719f3b0ce97b0fd2
Santhoshraj KG <87854476+SanthoshrajKG@users.noreply.github.com>
2022-11-12 12:00:42 +0530

    Update bubble_sort.py

--- sorting algorithms/bubble_sort.py
+++ sorting algorithms/bubble_sort.py
@@ -2,9 +2,8 @@ lst=[5,3,1,7,9,2]
 l=len(lst)
 for i in range(l):
   for j in range(l):
-    if(lst[i]>lst[j]):
+    if(lst[i]<=lst[j]):
       temp=lst[i]
       lst[i]=lst[j]
       lst[j]=temp
 print(lst)
-    
